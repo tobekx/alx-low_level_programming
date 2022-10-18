5fdced4b2955abaa0e5f473cc83fc79bac7a3771
tobekx <43343954+tobekx@users.noreply.github.com>
2022-10-18 16:07:12 +0100

    First program I know very well

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/*
+ * main - print alphabet
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	char l;
+	for (l = 'a'; l <= 'z'; l++)
+		_putchar(l);
+	_putchar('\n');
+}
